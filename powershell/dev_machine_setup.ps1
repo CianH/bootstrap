792cf27e1aa029e5aa841016e9859a13498ccc81
CianH <zerophyte@gmail.com>
2016-04-06 02:37:21 -0700

    Enable Bash on Ubuntu on Windows

--- powershell/dev_machine_setup.ps1
+++ powershell/dev_machine_setup.ps1
@@ -1,7 +1,15 @@
 # Dev Machine Powershell setup - Requires Admin prompt
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+	$arguments = "& '" + $myinvocation.mycommand.definition + "'"
+	Start-Process powershell -Verb runAs -ArgumentList $arguments
+	Break
+}
 
 # Chocolatey install
-iex ((new-object net.webclient).DownloadString('http://bit.ly/psChocInstall'))
+if (!(gcm cinst -ErrorAction SilentlyContinue)){
+	iex ((new-object net.webclient).DownloadString('http://bit.ly/psChocInstall'))
+}
 
 # install applications
 cinst slack -y
@@ -16,7 +24,10 @@ cinst putty -y
 cinst nuget.commandline -y
 
 # Setup Powershell symlinks
-cmd /c mklink /D  "$env:USERPROFILE\Documents\WindowsPowerShell\" "$PSScriptRoot"
+cmd /c mklink /D "$env:USERPROFILE\Documents\WindowsPowerShell\" "$PSScriptRoot"
 
 # Setup vimrc
 cmd /c mklink "$env:USERPROFILE\_vimrc" "$((Get-Item $PSScriptRoot).parent.FullName)\.vimrc"
+
+# Enable Windows Optional Features
+try { Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Windows-Subsystem-Linux" } catch {} # Fails on TH2, swallow the error.
