dcd6761f9f1c8d73187ef7a125bbbd98764f6c7e
CianH <zerophyte@gmail.com>
2018-03-07 15:20:36 -0800

    Also setup bash_aliases

--- bash_setup.sh
+++ bash_setup.sh
@@ -4,11 +4,13 @@ BASEDIR=`dirname $0`
 
 [ -e $HOME/.profile ] && mv $HOME/.profile $HOME/.profile.old
 [ -e $HOME/.bashrc ] && mv $HOME/.bashrc $HOME/.bashrc.old
+[ -e $HOME/.bash_aliases ] && mv $HOME/.bash_aliases $HOME/.bash_aliases.old
 [ -e $HOME/.vimrc ] && mv $HOME/.vimrc $HOME/.vimrc.old
 [ -e $HOME/.inputrc ] && mv $HOME/.inputrc $HOME/.inputrc.old
 
 ln -s $BASEDIR/.profile $HOME/.profile
 ln -s $BASEDIR/.bashrc $HOME/.bashrc
+ln -s $BASEDIR/.bash_aliases $HOME/.bash_aliases
 ln -s $BASEDIR/.vimrc $HOME/.vimrc
 ln -s $BASEDIR/.inputrc $HOME/.inputrc
 
