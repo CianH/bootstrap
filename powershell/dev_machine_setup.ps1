a672064325277bd4244cb6da0de9dec190524d94
Cian Hannafin <CianH@users.noreply.github.com>
2018-04-19 21:23:00 -0700

    update dev machine setup

--- powershell/dev_machine_setup.ps1
+++ powershell/dev_machine_setup.ps1
@@ -8,18 +8,14 @@ if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdenti
 
 # Chocolatey install
 if (!(gcm cinst -ErrorAction SilentlyContinue)){
-	iex ((new-object net.webclient).DownloadString('http://bit.ly/psChocInstall'))
+	iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 }
 
 # install applications
 cinst slack -y
-cinst notepadplusplus -y
 cinst github -y
-cinst google-chrome-x64 -y
 cinst firefox -y
-cinst vim -y
 cinst sysinternals -y
-cinst putty -y
 cinst nuget.commandline -y
 
 # Setup Powershell symlinks
@@ -27,9 +23,6 @@ cmd /c mklink /D "$env:USERPROFILE\Documents\WindowsPowerShell\" "$PSScriptRoot"
 
 # Setup vimrc
 cmd /c mklink "$env:USERPROFILE\_vimrc" "$((Get-Item $PSScriptRoot).parent.FullName)\.vimrc"
-
-# Enable Windows Optional Features
-try { Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Windows-Subsystem-Linux" } catch {} # Fails on TH2, swallow the error.
 
 # Set privacy settings
 & $PSScriptRoot\privacy_settings.ps1
