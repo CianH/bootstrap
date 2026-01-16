5c23be42fda5ed54cf604f1557013a3ad780e9e7
Cian Hannafin <1442396+CianH@users.noreply.github.com>
2026-01-16 17:51:23 -0600

    Reorganize registry settings into win/regkeys folder
    - Create win/regkeys/ folder for Windows registry files
    - Split privacy settings into individual .reg files with comments
    - Delete obsolete DisableCortana.reg (Cortana removed in Win11)
    - Update privacy_settings.ps1 to import .reg files

--- powershell/privacy_settings.ps1
+++ powershell/privacy_settings.ps1
@@ -1,12 +1,25 @@
-### Configure settings ###
-sp -path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search -name BingSearchEnabled -value 0 # Web Search in Start
-sp -path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\DataCollection -name AllowTelemetry -value 0 # Telemetry
+# Apply privacy-related registry settings
+# Each .reg file in win/regkeys/ contains a single setting with comments explaining its purpose
 
-# Privacy/General
-sp -path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\AdvertisingInfo -name Enabled -value 0 # Let apps use my advertising ID
-sp -path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppHost -name EnableWebContentEvaluation -value 0 # SmartScreen Filter
-sp -path HKCU:\SOFTWARE\Microsoft\Input\TIPC -name Enabled -value 0 # Send MSFT info about typing/writing
-sp -path "HKCU:\Control Panel\International\User Profile\" -name HttpAcceptLanguageOptOut -value 1 # Share language list to websites (1 is disable)
+$regkeysPath = Join-Path $PSScriptRoot "..\win\regkeys"
 
-# Privacy/Location
-sp -path HKLM:\SOFTWARE\Microsoft\PolicyManager\current\device\System -name AllowLocation -value 0 # Location for this device
+$regFiles = @(
+    "DisableAdvertisingId.reg",
+    "DisableBingSearch.reg",
+    "DisableLanguageSharing.reg",
+    "DisableLocation.reg",
+    "DisableOnedriveAds.reg",
+    "DisableSmartScreen.reg",
+    "DisableTelemetry.reg",
+    "DisableTypingData.reg"
+)
+
+foreach ($file in $regFiles) {
+    $path = Join-Path $regkeysPath $file
+    if (Test-Path $path) {
+        Write-Host "Applying $file..." -ForegroundColor Cyan
+        reg import $path 2>$null
+    } else {
+        Write-Warning "Registry file not found: $path"
+    }
+}
